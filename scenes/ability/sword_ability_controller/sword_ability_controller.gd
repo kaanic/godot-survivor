55d8eec3134075b60555965e93fd4240665ecf1d
Kaan Özkilic <kaanic1337@gmail.com>
2025-03-11 10:00:13 +0200

    improved map, new ability added

--- scenes/ability/sword_ability_controller/sword_ability_controller.gd
+++ scenes/ability/sword_ability_controller/sword_ability_controller.gd
@@ -46,6 +46,6 @@ func on_ability_upgrade_added(upgrade: AbilityUpgrade, current_upgrades: Diction
 	if upgrade.id != "sword_rate":
 		return
 	
-	var percent_reduction = current_upgrades["sword_rate"]["quantity"] * .15
+	var percent_reduction = current_upgrades["sword_rate"]["quantity"] * .1
 	$Timer.wait_time = base_wait_time * (1 - percent_reduction)
 	$Timer.start()
